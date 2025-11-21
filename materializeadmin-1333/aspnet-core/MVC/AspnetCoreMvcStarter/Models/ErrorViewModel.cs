7f6886f0a51f8cec7166fbbee7ff89eec6305d89
mohammed3738 <baigm066@gmail.com>
2025-11-21 15:41:16 +0530

    Remove hrms_copy and materializeadmin-1333 folders

--- materializeadmin-1333/aspnet-core/MVC/AspnetCoreMvcStarter/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace AspnetCoreMvcStarter.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}

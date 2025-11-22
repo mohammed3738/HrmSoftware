ea49534cdcb8c86a023c76b900d64146ffbfcaee
mohammed3738 <baigm066@gmail.com>
2025-11-22 16:46:27 +0530

    Update ALLOWED_HOSTS to allow all hosts in development

--- hrms/hrms/settings.py
+++ hrms/hrms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7o9%azh!(65zeu619gay11-&^5kzime&x@o71gc_!o&s*ed$=#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
